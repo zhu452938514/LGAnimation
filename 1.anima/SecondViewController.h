3858455a14246d577e0dded200a2f64c183fc774
OTC <otc@OTCdeMac-mini.local>
2016-10-13 14:43:14 +0800

    boss直聘的转场动画实现

---
+++ 1.anima/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  1.anima
+//
+//  Created by OTC on 16/10/13.
+//  Copyright © 2016年 OTC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
